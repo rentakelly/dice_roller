f8fa7d47a4db3574c8da50a927d3abf436a5c276
Renata Kelly <renakelly@gmail.com>
2021-02-24 14:21:54 -0300

    atualiza o gradle

--- app/src/main/java/br/com/diceroller/MainActivity.kt
+++ app/src/main/java/br/com/diceroller/MainActivity.kt
@@ -1,11 +1,10 @@
 package br.com.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
